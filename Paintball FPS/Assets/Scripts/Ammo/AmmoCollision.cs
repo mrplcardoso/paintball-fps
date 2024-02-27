0ca6274740290753f0d244610ee724fec5ab0a7d
mrplcardoso <mrplcardoso@gmail.com>
2024-02-27 19:55:25 -0300

    Efeito de tinta ao colidir projetil com objetos com o shader para tal

--- Paintball FPS/Assets/Scripts/Ammo/AmmoCollision.cs
+++ Paintball FPS/Assets/Scripts/Ammo/AmmoCollision.cs
@@ -8,6 +8,8 @@ public class AmmoCollision : MonoBehaviour
 	UnitAttributes attributes;
 	MatchColor match;
 	[SerializeField] IPoolableObject poolable;
+	[SerializeField] SplashParticle particle;
+	Vector3 size;
 
 	float damage => attributes[AttributeType.Damage];
 		
@@ -16,10 +18,19 @@ public class AmmoCollision : MonoBehaviour
 		attributes = GetComponentInChildren<UnitAttributes>();
 		match = GetComponentInChildren<MatchColor>();
 		poolable = GetComponentInChildren<IPoolableObject>();
+		size = GetComponentInChildren<Renderer>().bounds.size;
+		particle = Instantiate(particle, transform.position, Quaternion.identity);
  	}
 	
 	void OnTriggerEnter(Collider collider)
 	{
+		Color c = GetComponent<Renderer>().material.color;
+
+		if(collider.gameObject.TryGetComponent<Paintable>(out Paintable p))
+		{
+			p.Paint(c, transform.position, size);
+		}
+
 		if(collider.gameObject.TryGetComponent<TakeDamage>(out TakeDamage t))
 		{
 			if(collider.gameObject.TryGetComponent<MatchColor>(out MatchColor m))
@@ -27,6 +38,11 @@ public class AmmoCollision : MonoBehaviour
 				if(m.color == match.color) { t.Damage(damage); }
 			}
 		}
+
+		particle.SetColor(c);
+		particle.transform.position = transform.position;
+		particle.SetActive(true);
+		
 		poolable.Deactivate();
 	}
 }
